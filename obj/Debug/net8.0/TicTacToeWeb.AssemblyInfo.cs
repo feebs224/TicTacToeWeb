4f467428e99ae0c5fdef11a3bb3aff5132b489e5
feebs224 <fibi057057@gmail.com>
2024-05-20 14:37:37 -0500

    Initial commit

--- obj/Debug/net8.0/TicTacToeWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/TicTacToeWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3981473f218aacb0163c5ae74017fa03294f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91998f4da7168452d8d1fb6ed09c9bd6d57b1ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
